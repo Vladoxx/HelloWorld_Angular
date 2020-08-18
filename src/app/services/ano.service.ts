5c9ca219b922a3243de38e83c9835adc8046663c
Vladoxx <wladox@outlook.com>
2020-08-18 02:32:47 -0300

    Introdução a Angula

---
+++ src/app/services/ano.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnoService {
+
+  constructor() { }
+
+  getAno(): number {
+    return new Date().getFullYear();
+  }
+
+}
